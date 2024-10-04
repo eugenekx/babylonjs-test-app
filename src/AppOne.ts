362c7f884f62e867f3da8e9d9f25b903883a301d
eugenekx <kx.eugene@gmail.com>
2024-10-04 18:11:45 +0300

    third-person character demo

--- src/AppOne.ts
+++ src/AppOne.ts
@@ -1,94 +1,145 @@
-import * as BABYLON from 'babylonjs'
-export class AppOne {
-    engine: BABYLON.Engine;
-    scene: BABYLON.Scene;
+import * as BABYLON from "babylonjs";
+import { CharacterController } from "babylonjs-charactercontroller";
+
+export class App {
+    private _engine: BABYLON.Engine;
+    private _scene: BABYLON.Scene;
 
     constructor(readonly canvas: HTMLCanvasElement) {
-        this.engine = new BABYLON.Engine(canvas)
-        window.addEventListener('resize', () => {
-            this.engine.resize();
+        this._engine = new BABYLON.Engine(canvas);
+        window.addEventListener("resize", () => {
+            this._engine.resize();
         });
-        this.scene = createScene(this.engine, this.canvas)
-
+        this._scene = createScene(this._engine, this.canvas);
     }
 
     debug(debugOn: boolean = true) {
         if (debugOn) {
-            this.scene.debugLayer.show({ overlay: true });
+            this._scene.debugLayer.show({ overlay: true });
         } else {
-            this.scene.debugLayer.hide();
+            this._scene.debugLayer.hide();
         }
     }
 
     run() {
         this.debug(true);
-        this.engine.runRenderLoop(() => {
-            this.scene.render();
-        });
     }
-
-
-
 }
 
+function createScene(engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
+    const scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color4(0.75, 0.75, 0.75, 1);
+    scene.ambientColor = new BABYLON.Color3(1, 1, 1);
 
-var createScene = function (engine: BABYLON.Engine, canvas: HTMLCanvasElement) {
-    // this is the default code from the playground:
+    scene.createDefaultEnvironment();
+    scene.createDefaultLight();
 
-    // This creates a basic Babylon Scene object (non-mesh)
-    var scene = new BABYLON.Scene(engine);
+    createGround(scene);
 
-    // This creates and positions a free camera (non-mesh)
-    var camera = new BABYLON.FreeCamera("camera1", new BABYLON.Vector3(0, 5, -10), scene);
-
-    // This targets the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-
-    // This attaches the camera to the canvas
-    camera.attachControl(canvas, true);
-
-    // This creates a light, aiming 0,1,0 - to the sky (non-mesh)
-    var light = new BABYLON.HemisphericLight("light", new BABYLON.Vector3(0, 1, 0), scene);
-
-    // Default intensity is 1. Let's dim the light a small amount
-    light.intensity = 0.7;
-
-    // Our built-in 'sphere' shape.
-    var sphere = BABYLON.MeshBuilder.CreateSphere("sphere", { diameter: 2, segments: 32 }, scene);
-    // Move the sphere upward 1/2 its height
-    let startPos = 2;
-    sphere.position.y = startPos;
-
-    // Our built-in 'ground' shape.
-    var ground = BABYLON.MeshBuilder.CreateGround("ground", { width: 6, height: 6 }, scene);
-    var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
-    groundMaterial.diffuseColor = new BABYLON.Color3(0.5, 0.8, 0.5); // RGB for a greenish color
-    ground.material = groundMaterial;
-    groundMaterial.bumpTexture = new BABYLON.Texture("./normal.jpg", scene);
-    //groundMaterial.bumpTexture.level = 0.125;    
-
-
-    var redMaterial = new BABYLON.StandardMaterial("redMaterial", scene);
-    redMaterial.diffuseColor = new BABYLON.Color3(1, 0, 0); // RGB for red
-    sphere.material = redMaterial;
-
-    var sphereVelocity = 0;
-    var gravity = 0.009;
-    var reboundLoss = 0.1;
-
-    scene.registerBeforeRender(() => {
-        sphereVelocity += gravity;
-        let newY = sphere.position.y - sphereVelocity;
-        sphere.position.y -= sphereVelocity
-        if (newY < 1) {
-            sphereVelocity = (reboundLoss - 1) * sphereVelocity;
-            newY = 1;
-        }
-        sphere.position.y = newY;
-        if (Math.abs(sphereVelocity) <= gravity && newY < 1 + gravity) {
-            sphere.position.y = startPos++;
-        }
-    });
+    loadPlayer(scene, engine, canvas);
 
     return scene;
-};
+}
+
+function loadPlayer(
+    scene: BABYLON.Scene,
+    engine: BABYLON.Engine,
+    canvas: HTMLCanvasElement
+) {
+    BABYLON.SceneLoader.ImportMesh(
+        "",
+        "assets/player/",
+        "Vincent.babylon",
+        scene,
+        (meshes, particleSystems, skeletons) => {
+            const player = meshes[0];
+            const skeleton = skeletons[0];
+            player.skeleton = skeleton;
+
+            skeleton.enableBlending(0.1);
+
+            const sm = <BABYLON.StandardMaterial>player.material;
+            if (sm.diffuseTexture != null) {
+                sm.backFaceCulling = true;
+                sm.ambientColor = new BABYLON.Color3(1, 1, 1);
+            }
+
+            player.position = new BABYLON.Vector3(0, 12, 0);
+            player.checkCollisions = true;
+            player.ellipsoid = new BABYLON.Vector3(0.5, 1, 0.5);
+            player.ellipsoidOffset = new BABYLON.Vector3(0, 1, 0);
+
+            //rotate the camera behind the player
+            const alpha = -player.rotation.y - 4.69;
+            const beta = Math.PI / 2.5;
+            const target = new BABYLON.Vector3(
+                player.position.x,
+                player.position.y + 1.5,
+                player.position.z
+            );
+
+            const camera = new BABYLON.ArcRotateCamera(
+                "ArcRotateCamera",
+                alpha,
+                beta,
+                5,
+                target,
+                scene
+            );
+
+            camera.wheelPrecision = 15;
+            camera.checkCollisions = false;
+
+            camera.keysLeft = [];
+            camera.keysRight = [];
+            camera.keysUp = [];
+            camera.keysDown = [];
+
+            camera.lowerRadiusLimit = 2;
+
+            camera.upperRadiusLimit = 20;
+            camera.attachControl(canvas, false);
+
+            const cc = new CharacterController(
+                <BABYLON.Mesh>player,
+                camera,
+                scene
+            );
+
+            cc.setCameraTarget(new BABYLON.Vector3(0, 1.5, 0));
+
+            cc.setNoFirstPerson(false);
+            cc.setStepOffset(0.4);
+            cc.setSlopeLimit(30, 60);
+
+            cc.setIdleAnim("idle", 1, true);
+            cc.setTurnLeftAnim("turnLeft", 0.5, true);
+            cc.setTurnRightAnim("turnRight", 0.5, true);
+            cc.setWalkBackAnim("walkBack", 0.5, true);
+            cc.setIdleJumpAnim("idleJump", 0.5, false);
+            cc.setRunJumpAnim("runJump", 0.6, false);
+            cc.setFallAnim(null, 2, false);
+            cc.setSlideBackAnim("slideBack", 1, false);
+
+            cc.start();
+
+            engine.runRenderLoop(function () {
+                scene.render();
+            });
+        }
+    );
+}
+
+function createGround(scene: BABYLON.Scene) {
+    const grnd = BABYLON.CreateGround(
+        "ground",
+        { width: 128, height: 128 },
+        scene
+    );
+    grnd.checkCollisions = true;
+
+    const gMat = new BABYLON.StandardMaterial("groundMat", scene);
+    gMat.alpha = 0.0;
+
+    grnd.material = gMat;
+}
